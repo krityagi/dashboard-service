b92408c7389c1975e71690df7507d9645fbf5837
krityagi <ktyagi0602@gmail.com>
2025-03-10 00:35:32 +0000

    updated

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 function isAuthenticated(req, res, next) {
     //console.log('Request Headers:', req.headers);
